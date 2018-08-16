1dea9155930af9bf3af4bdd3ba862cf07747a7d8
Tran Viet Dung <dungviettran89@gmail.com>
2018-08-16 20:51:23 +0800

    Added base code for wotlk-ai

---
+++ base/build.sh
@@ -0,0 +1,2 @@
+docker build -t radiocuatoi/apps:trinitycore-wotlk-base-latest .
+docker push radiocuatoi/apps:trinitycore-wotlk-base-latest
---
+++ builder/build.sh
@@ -0,0 +1,2 @@
+docker build -t radiocuatoi/apps:trinitycore-wotlk-builder-latest .
+docker push radiocuatoi/apps:trinitycore-wotlk-builder-latest
