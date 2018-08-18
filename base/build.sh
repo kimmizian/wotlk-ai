f6606a58aa785b2bf2f68ea5c8a2704898022787
Tran Viet Dung <dungviettran89@gmail.com>
2018-08-18 08:19:18 +0800

    Initialize all information

--- base/build.sh
+++ base/build.sh
@@ -1,2 +1,2 @@
-docker build -t radiocuatoi/apps:trinitycore-wotlk-base-latest .
+docker build -t radiocuatoi/apps:trinitycore-wotlk-base-latest . &&\
 docker push radiocuatoi/apps:trinitycore-wotlk-base-latest
---
+++ database/00-create-databases.sql
@@ -0,0 +1,3 @@
+create database if not exists auth;
+create database if not exists world;
+create database if not exists characters;
